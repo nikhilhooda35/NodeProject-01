c55389662b81d6e9ec78c11fe6fc75c4c7d0fdf8
Nikhil Hooda <nikhil.hooda@uneecops.com>
2024-06-03 12:47:41 +0530

    using api & rendering html

--- index.js
+++ index.js
@@ -4,8 +4,17 @@ const app = express();
 const PORT = 8000;
 
 // Routes
-app.get("/users", (req, res) => {
+app.get("/api/users", (req, res) => {
   return res.json(users);
+});
+
+app.get("/users", (req, res) => {
+  const html = `
+   <ul>
+   ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
+   </ul>
+   `;
+  res.send(html);
 });
 
 app.listen(PORT, () => console.log(`Server Started at Port: ${PORT}`));
