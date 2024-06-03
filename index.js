98a64183bf8572cb3404f9eaa11350b207914c83
Nikhil Hooda <nikhil.hooda@uneecops.com>
2024-06-03 20:51:13 +0530

    api versioning

--- index.js
+++ index.js
@@ -4,17 +4,28 @@ const app = express();
 const PORT = 8000;
 
 // Routes
+app.get("/users", (req, res) => {
+  const html = `
+     <ul>
+     ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
+     </ul>
+     `;
+  res.send(html);
+});
+
+// REST API
 app.get("/api/users", (req, res) => {
   return res.json(users);
 });
 
-app.get("/users", (req, res) => {
-  const html = `
-   <ul>
-   ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
-   </ul>
-   `;
-  res.send(html);
+app.get("/api/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const user = users.find((user) => user.id === id);
+  return res.json(user);
 });
+
+app.post('/api/users', (req, res) => {
+    
+})
 
 app.listen(PORT, () => console.log(`Server Started at Port: ${PORT}`));
